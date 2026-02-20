8142aa85170203ed8fc322a5b1a906c858b8d760
Rutvik-090 <rutvikkumar18319@gmail.com>
2026-02-21 01:42:21 +0530

    first commit

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  useEffect(() => {
+    if (token) {
+      console.log("token exists");
+    }
+  }, [token]);
+
+  const login = (userData, tokenValue) => {
+    localStorage.setItem("token", tokenValue);
+    setUser(userData);
+    setToken(tokenValue);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+    setToken(null);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, token, login, logout }}
+    ></AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
