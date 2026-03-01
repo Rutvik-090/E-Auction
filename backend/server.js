d05f773b48f988b99d5b7823ef699be30a2fd3da
Rutvik-090 <rutvikkumar18319@gmail.com>
2026-03-01 23:54:34 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,26 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
-
-const PORT = process.env.PORT || 5001;
+connectDB();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API working.");
 });
 
 app.use("/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
