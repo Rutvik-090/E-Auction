cbcbb7cd86031d6e64ee117e2b06b51f95e2b2db
Rutvik-090 <rutvikkumar18319@gmail.com>
2026-03-05 23:09:13 +0530

    added function to delete auction

--- backend/controllers/auctionController.js
+++ backend/controllers/auctionController.js
@@ -124,3 +124,38 @@ export const updateAuction = async (req, res) => {
     });
   }
 };
+
+//-----------------
+//Delete auction
+//-----------------
+export const deleteAuction = async (req, res) => {
+  try {
+    const auction = await Auction.findById(req.params.id);
+
+    if (!auction) {
+      return res.status(404).json({
+        success: false,
+        message: "Auction not found",
+      });
+    }
+
+    if (auction.seller.toString() !== req.user._id.toString()) {
+      return res.status(403).json({
+        success: false,
+        message: "Not authorized",
+      });
+    }
+
+    await auction.deleteOne();
+
+    res.status(200).json({
+      success: true,
+      message: "Auction deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
