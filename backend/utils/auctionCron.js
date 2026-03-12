f6f7a0f1c4781c393b191e47ca89ecfdb29fae18
Rutvik-090 <rutvikkumar18319@gmail.com>
2026-03-13 00:14:13 +0530

    added auctionCron file to detect automatic winners

---
+++ backend/utils/auctionCron.js
@@ -0,0 +1,34 @@
+import cron from "node-cron";
+import Auction from "../models/Auction.js";
+import Bid from "../models/Bid.js";
+
+export const startAuctionCron = () => {
+  // Runs every minute
+  cron.schedule("* * * * *", async () => {
+    const now = new Date();
+
+    const expiredAuctions = await Auction.find({
+      endTime: { $lt: now },
+      status: "active",
+    });
+
+    for (const auction of expiredAuctions) {
+      // Find highest bid
+      const highestBid = await Bid.findOne({ auction: auction._id })
+        .sort({ amount: -1 })
+        .populate("bidder");
+
+      if (highestBid) {
+        auction.status = "ended";
+        auction.winner = highestBid.bidder._id;
+        auction.finalPrice = highestBid.amount;
+      } else {
+        auction.status = "ended";
+      }
+
+      await auction.save();
+
+      console.log(`Auction ended: ${auction.title}`);
+    }
+  });
+};
